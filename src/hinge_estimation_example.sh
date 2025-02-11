c98e74fa7cd3253a0f24c43efd5da31bdbb15008
KoyanoBunsho <buna40950@gmail.com>
2025-02-11 16:25:12 +0900

    hinge estimation examples

--- src/hinge_estimation_example.sh
+++ src/hinge_estimation_example.sh
@@ -1,16 +1,12 @@
 echo 'adenylate kinase'
 
-docker exec mplh ./estimate_hinge_numbers adenylate_kinase/1ake.pdb adenylate_kinase/4ake.pdb A A bic exact
-docker exec mplh ./estimate_hinge_numbers adenylate_kinase/1ake.pdb adenylate_kinase/4ake.pdb A A bic lh
+docker exec shilo ./execute_sh_ilo adenylate_kinase/1ake.pdb adenylate_kinase/4ake.pdb A A 4
 
 echo 'short linker loop'
-docker exec mplh ./estimate_hinge_numbers linker/1mdt.pdb linker/1ddt.pdb A A bic exact 
-docker exec mplh ./estimate_hinge_numbers linker/1mdt.pdb linker/1ddt.pdb A A bic lh
+docker exec shilo ./execute_sh_ilo linker/1mdt.pdb linker/1ddt.pdb A A 2
 
 echo 'long linker loop'
-docker exec mplh ./estimate_hinge_numbers linker/1nkh.pdb linker/1fgx.pdb B A bic exact
-docker exec mplh ./estimate_hinge_numbers linker/1nkh.pdb linker/1fgx.pdb B A bic lh
+docker exec shilo ./execute_sh_ilo linker/1nkh.pdb linker/1fgx.pdb B A 2
 
 echo 'domain swapping'
-docker exec mplh ./estimate_hinge_numbers 2oct.pdb pdb4n6v.pdb A A bic exact
-docker exec mplh ./estimate_hinge_numbers 2oct.pdb pdb4n6v.pdb A A bic lh
+docker exec shilo ./execute_sh_ilo 2oct.pdb pdb4n6v.pdb A A 3
