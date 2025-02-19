65d978dc9d3385858cd0bdf57302ecf3f84bd470
KoyanoBunsho <buna40950@gmail.com>
2025-02-19 16:13:17 +0900

    simulation

---
+++ src/simulation_experiment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+g++ simulation_sh_ilo.cpp -o simulation_sh_ilo -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
+
+sigma_val=(0.5 1.0 1.5 2.0 2.5 3.0)
+model_type=("shilo" "sh" "shibuya")
+
+for sigma in "${sigma_val[@]}"; do
+    for hinge_num in {2..10}; do
+        for model in "${model_type[@]}"; do
+            ./simulation_sh_ilo "$hinge_num" "$model" "$sigma"
+        done
+    done
+done
