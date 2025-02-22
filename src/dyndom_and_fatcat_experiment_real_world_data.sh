79c74386ee7c07c61b53caa64c0342ee07062be6
KoyanoBunsho <buna40950@gmail.com>
2025-02-22 14:34:30 +0900

    add dyndom and fatcat experiment

--- src/dyndom_and_fatcat_experiment_real_world_data.sh
+++ src/dyndom_and_fatcat_experiment_real_world_data.sh
@@ -4,6 +4,6 @@ python dyndom_experiment_comp_time.py --csv_file pdb_with_hinges.csv --output dy
 python dyndom_experiment_comp_time.py --csv_file DynDon_database.csv --output dyndom_execution_time_dyn_improved.csv
 
 # FATCAT experiment
-python fatcat_experiment_comp_time.py --csv_file pdb_12_with_hinges_lower.csv --output dyndom_execution_time_shibuya_improved.csv
-python fatcat_experiment_comp_time.py --csv_file pdb_with_hinges.csv --output dyndom_execution_time_par_improved.csv
-python fatcat_experiment_comp_time.py --csv_file DynDon_database.csv --output dyndom_execution_time_dyn_improved.csv
+python fatcat_experiment_comp_time.py --csv_file pdb_12_with_hinges_lower.csv --output fatcat_execution_time_shibuya_improved.csv
+python fatcat_experiment_comp_time.py --csv_file pdb_with_hinges.csv --output fatcat_execution_time_par_improved.csv
+python fatcat_experiment_comp_time.py --csv_file DynDon_database.csv --output fatcat_execution_time_dyn_improved.csv
