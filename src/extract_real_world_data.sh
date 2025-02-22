fa9c9ea8a5c6ed5dbf8e47b957b3f7771383d4b3
KoyanoBunsho <buna40950@gmail.com>
2025-02-22 17:40:12 +0900

    add a real world data

--- src/extract_real_world_data.sh
+++ src/extract_real_world_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 g++ real_world_data_for_expeperiment.cpp -o real_world_data_for_expeperiment -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
-./real_world_data_for_expeperiment "coord_csv" "pdb_with_hinges.csv" "rmsdh_result/par_data_for_experiment.csv"
-./real_world_data_for_expeperiment "coord_csv_dyndom" "DynDom_database.csv" "rmsdh_result/dyn_data_for_experiment.csv"
+./real_world_data_for_expeperiment "coord_csv/" "pdb_with_hinges.csv" "rmsdh_result/par_data_for_experiment.csv"
+./real_world_data_for_expeperiment "coord_csv_dyndom/" "DynDom_database.csv" "rmsdh_result/dyn_data_for_experiment.csv"
