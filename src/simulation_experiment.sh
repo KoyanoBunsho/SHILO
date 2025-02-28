fbe431dd97a6ea2475db3bd2fdc1d3515e2360f1
KoyanoBunsho <buna40950@gmail.com>
2025-02-28 17:31:10 +0900

    simulation

--- src/simulation_experiment.sh
+++ src/simulation_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python make_simulation_data.py
-g++ output_simulation_file.cpp -o output_simulation_file -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
-./output_simulation_file
+#python make_simulation_data.py
+#g++ output_simulation_file.cpp -o output_simulation_file -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
+#./output_simulation_file
 
 
 g++ simulation_sh_ilo.cpp -o simulation_sh_ilo -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
@@ -9,7 +9,8 @@ g++ simulation_sh.cpp -o simulation_sh -std=c++14 -lstdc++fs -Wall -Wextra -O3 -
 g++ simulation_r_lo.cpp -o simulation_r_lo -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
 g++ simulation_r_ilo.cpp -o simulation_r_ilo -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
 
-sigma_val=(0.5 1.0 1.5)
+#sigma_val=(0.5 1.0 1.5)
+sigma_val=(0.5)
 model_type=("shlo" "sh" "shibuya")
 
 for sigma in "${sigma_val[@]}"; do
@@ -28,7 +29,7 @@ for sigma in "${sigma_val[@]}"; do
     done
 done
 
-bash fatcat_dyndom_simulation.sh
-bash fatcat_dyndom_result.sh
+#bash fatcat_dyndom_simulation.sh
+#bash fatcat_dyndom_result.sh
 
 python concat_simulation_res.py
