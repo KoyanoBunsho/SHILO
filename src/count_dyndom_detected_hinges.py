6fd4b1716876f5ee075e2e57048601751b85e87a
KoyanoBunsho <buna40950@gmail.com>
2025-03-05 23:09:04 +0900

    output simulation

--- src/count_dyndom_detected_hinges.py
+++ src/count_dyndom_detected_hinges.py
@@ -3,34 +3,35 @@ import pandas as pd
 from tqdm import tqdm
 import re
 import numpy as np
-from biopandas.pdb import PandasPdb  # biopandasのインポート
+from biopandas.pdb import PandasPdb
+from joblib import Parallel, delayed
+
+
+def process_row(row):
+    p_pdb_path = row["p_pdb"]
+    ppdb = PandasPdb().read_pdb(p_pdb_path)
+    unique_res_nums = list(set(ppdb.df["ATOM"]["residue_number"]))
+    res_mapping = {res: idx + 1 for idx, res in enumerate(unique_res_nums)}
+    pdb1_basename = os.path.basename(p_pdb_path).replace(".pdb", "")
+    pdb2_basename = os.path.basename(row["q_pdb"]).replace(".pdb", "")
+    res_file = f"{pdb1_basename}_{pdb2_basename}_info"
+    hinge_positions = extract_hinge_positions(res_file)
+    mapped_hinge_positions = [res_mapping.get(pos, pos) for pos in hinge_positions]
+    mapped_hinge_positions_str = " : ".join(map(str, mapped_hinge_positions))
+    hinge_count = count_bending_residues(res_file)
+
+    hinge_info = row["hinge_file_path"]
+    return mapped_hinge_positions_str, hinge_count, hinge_info
 
 
 def main():
     simulation_file = "output_simulation_file.csv"
     df = pd.read_csv(simulation_file)
-    hinge_count_list = []
-    hinge_info_list = []
-    hinge_positions_list = []
-
-    for _, row in tqdm(df.iterrows(), total=len(df)):
-        p_pdb_path = row["p_pdb"]
-        ppdb = PandasPdb().read_pdb(p_pdb_path)
-        unique_res_nums = list(set(ppdb.df["ATOM"]["residue_number"]))
-        res_mapping = {res: idx + 1 for idx, res in enumerate(unique_res_nums)}
-
-        pdb1_basename = os.path.basename(p_pdb_path).replace(".pdb", "")
-        pdb2_basename = os.path.basename(row["q_pdb"]).replace(".pdb", "")
-        res_file = f"{pdb1_basename}_{pdb2_basename}_info"
-
-        hinge_positions = extract_hinge_positions(res_file)
-        mapped_hinge_positions = [res_mapping.get(pos, pos) for pos in hinge_positions]
-        mapped_hinge_positions_str = " : ".join(map(str, mapped_hinge_positions))
-        hinge_positions_list.append(mapped_hinge_positions_str)
-
-        hinge_count = count_bending_residues(res_file)
-        hinge_count_list.append(hinge_count)
-        hinge_info_list.append(row["hinge_file_path"])
+    records = df.to_dict("records")
+    results = Parallel(n_jobs=-1)(
+        delayed(process_row)(row) for row in tqdm(records, total=len(records))
+    )
+    hinge_positions_list, hinge_count_list, hinge_info_list = zip(*results)
 
     df["hinge_index"] = hinge_positions_list
     df["detected_hinge_count"] = hinge_count_list
