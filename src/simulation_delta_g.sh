628fa89f7263a34d3f2706d0c666d52adb9471e4
KoyanoBunsho <buna40950@gmail.com>
2025-04-26 15:17:00 +0900

    delta

--- src/simulation_delta_g.sh
+++ src/simulation_delta_g.sh
@@ -3,13 +3,18 @@
 #$ -cwd
 #$ -l s_vmem=2.1G
 #$ -pe mpi_32 128
-g++ calc_delta_g_simulation.cpp -o calc_delta_g_simulation -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
+g++ calc_delta_g.cpp -o calc_delta_g -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
+g++ calc_delta_g_par.cpp -o calc_delta_g_par -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
+g++ calc_delta_g_dyndom.cpp -o calc_delta_g_dyndom -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
+#g++ calc_delta_g_simulation.cpp -o calc_delta_g_simulation -std=c++14 -lstdc++fs -Wall -Wextra -O3 -mtune=native -march=native -mfpmath=both -Werror -fopenmp
 
+./calc_delta_g
+./calc_delta_g_par
+./calc_delta_g_dyndom
+#sigma_val=(0.5)
 
-sigma_val=(0.5)
-
-for sigma in "${sigma_val[@]}"; do
-    for hinge_num in {2..5}; do
-        ./calc_delta_g_simulation "$hinge_num" "$sigma"
-    done
-done
+#for sigma in "${sigma_val[@]}"; do
+ #   for hinge_num in {2..5}; do
+  #      ./calc_delta_g_simulation "$hinge_num" "$sigma"
+   # done
+#done
